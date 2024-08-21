22425515388dd6cd4a63b732a79a9a8a5857ecdf
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-08-21 03:04:33 -0600

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="bg-cover bg-center h-screen flex items-center justify-center" style="background-image: url('../img/parrilla.jpg');">
+<div class="bg-cover bg-center h-screen flex items-center justify-center rounded-xl" style="background-image: url('../img/parrilla.jpg');">
     <div class="container mx-auto px-4 py-16 md:py-24 animate-fadeInUp">
         <div class="flex flex-col md:flex-row md:space-x-8 items-center">
             <div class="md:w-1/2 flex justify-center">
-                <div class="md:w-1/2 w-32 h-32 md:w-64 md:h-64 flex justify-center items-center bg-white rounded-full shadow-lg transform hover:scale-105 transition-transform duration-300">
+                <div class="md:w-1/2 md:h-1/2 w-32 h-32 flex justify-center items-center bg-white rounded-full shadow-lg transform hover:scale-105 transition-transform duration-300">
                     <img src="{{ asset('img/logo.jpg') }}" class="w-full h-full object-cover rounded-full" alt="Logo Comedor Industrial">
                 </div>
             </div>
@@ -50,7 +50,7 @@
             </template>
         </div>
         <button @click="prevSlide()" id="prev" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 group-hover:bg-black/50 group-focus:ring-4 group-focus:ring-white">
                 <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
                 </svg>
@@ -58,7 +58,7 @@
             </span>
         </button>
         <button @click="nextSlide()" id="next" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 group-hover:bg-black/50 group-focus:ring-4 group-focus:ring-white">
                 <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                 </svg>
